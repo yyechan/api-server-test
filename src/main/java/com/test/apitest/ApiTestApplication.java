580e39fa5ead81c4d73cb19da7e174eded1757f7
yyechan <yyechan@naver.com>
2021-08-27 00:19:05 +0900

    2021-08-27 init

---
+++ src/main/java/com/test/apitest/ApiTestApplication.java
@@ -0,0 +1,14 @@
+package com.test.apitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+public class ApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestApplication.class, args);
+	}
+
+}
